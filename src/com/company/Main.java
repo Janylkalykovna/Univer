125ca0804cd2e3f1389d4d245eb42225955ef020
User <janylkalykovna>
2022-02-14 12:08:26 +0600

    1 tema

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Univer univ = new Univer();
+        univ.name="Kyrgyz state University";
+        univ.year=1991;
+        univ.status="Stste";
+
+    }
+}
